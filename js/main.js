41ab1bb91ac39158234d486654077a6ae3b5f519
che.doddington <che.doddington@gmail.com>
2015-07-30 03:17:36 -0400

    Finished map leaflet

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
-/* Begin by adding your on ready handler here, and then create the
-   rest of your functions inside the on ready handler.
+$(document).ready(function(){
+	var map = L.map('map-container').setView([46.855669, -121.767773], 9);
+	
+	var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+	});
 
-   (Note that you do not need to manually call Bootstrap functions in
-   your Javascript because Bootstrap will automatically recognize your
-   HTML structures and invoke the proper JS code accordingly. Be sure
-   to reference the Bootstrap documentation.)
-*/
-
-// TODO: Inside of your on ready handler, invoke the Leaflet.js library
-// to draw a map in your `#map-container` div.
-
-// TODO: Add 2 layers to your map you have visuals. Use the Open Street Maps
-// tiles served through the MapQuest CDN. Consult this example to set up
-// the map tiles layers:
-
-
-// TODO: Customize that Map to show markers with popups at no fewer than 3
-// interesting locations. (You'll need to figure out the latitude/longitude for
-// these locations using a mapping tool such as Google Maps.)
-
+	var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+	});
+	
+	var mapLayers = {
+		"Street Map" : drawLayer,
+		"Sattelite" : satLayer
+	};
+	
+	L.control.layers(mapLayers).addTo(map);
+	drawLayer.addTo(map);
+	
+	// 3 points of interest
+	var cougarRock = L.marker([46.740946, -121.918186]);
+	cougarRock.addTo(map);
+	
+	var reflectionLake = L.marker([46.769683, -121.730270]);
+	reflectionLake.addTo(map);
+	
+	var sprayPark = L.marker([46.915509, -121.824484]);
+	sprayPark.addTo(map);
+});
